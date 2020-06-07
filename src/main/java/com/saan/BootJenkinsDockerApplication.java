1d22f29a8ad45bf4a85ab9745416c91439646aeb
Sandeep Maurya <sandeep.maurya018@gmail.com>
2020-06-07 17:29:22 +0530

    first commit

---
+++ src/main/java/com/saan/BootJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.saan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJenkinsDockerApplication.class, args);
+	}
+
+}
